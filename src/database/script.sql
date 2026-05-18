9e9f2bb9d08066da03d9641c89bbed9a0a07f38b
IgorDias-dev <igor.reis@sptech.school>
2026-05-18 07:24:29 -0300

    Correção do banco de dados e DER

--- src/database/script.sql
+++ src/database/script.sql
@@ -1,38 +1,18 @@
 CREATE DATABASE IF NOT EXISTS plusTourists;
-USE plusTourists;
 
-DROP TABLE IF EXISTS log;
-DROP TABLE IF EXISTS registro_turismo;
-DROP TABLE IF EXISTS atividade;
-DROP TABLE IF EXISTS edicao;
-DROP TABLE IF EXISTS evento;
-DROP TABLE IF EXISTS organizador;
-DROP TABLE IF EXISTS plano_turistico;
-DROP TABLE IF EXISTS locall;
-DROP TABLE IF EXISTS tipo_transporte;
-DROP TABLE IF EXISTS pais_origem;
-DROP TABLE IF EXISTS status_plano;
-DROP TABLE IF EXISTS funcionario;
-DROP TABLE IF EXISTS estado;
-DROP TABLE IF EXISTS empresa;
+USE plusTourists;
 
 CREATE TABLE empresa (
     id_empresa INT PRIMARY KEY AUTO_INCREMENT,
-    nome_fantasia VARCHAR(200),
     razao_social VARCHAR(200) NOT NULL,
+    nome_fantasia VARCHAR(200),
     cnpj CHAR(14) UNIQUE NOT NULL,
-    dt_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
-    cep CHAR(9) NOT NULL,
-    complemento VARCHAR(50),
-    numero CHAR(6) NOT NULL,
     email_contato VARCHAR(150) NOT NULL,
-    codigo_ativacao CHAR(6) NOT NULL
-);
-
-CREATE TABLE estado (
-    id_estado INT PRIMARY KEY AUTO_INCREMENT,
-    nome_estado VARCHAR(100) NOT NULL DEFAULT 'SEM NOME',
-    UF CHAR(2) NOT NULL UNIQUE
+    cep CHAR(9) NOT NULL,
+    numero CHAR(8) NOT NULL,
+    complemento VARCHAR(100),
+    codigo_ativacao CHAR(6),
+    dt_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE funcionario (
@@ -40,100 +20,13 @@ CREATE TABLE funcionario (
     nome VARCHAR(100) NOT NULL,
     cpf CHAR(11) UNIQUE NOT NULL,
     email VARCHAR(200) NOT NULL,
-    senha VARCHAR(20) NOT NULL,
+    senha VARCHAR(100) NOT NULL,
     nivel_acesso INT NOT NULL,
     id_empresa INT NOT NULL,
-    FOREIGN KEY (id_empresa) REFERENCES empresa(id_empresa)
+    FOREIGN KEY (id_empresa) REFERENCES empresa (id_empresa)
 );
 
-CREATE TABLE status_plano (
-    id_status_plano INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(20) NOT NULL UNIQUE
-);
-
-CREATE TABLE locall (
-    id_local INT PRIMARY KEY AUTO_INCREMENT,
-    municipio VARCHAR(100) NOT NULL,
-    regiao_turistica VARCHAR(100),
-    id_estado INT NOT NULL,
-    FOREIGN KEY (id_estado) REFERENCES estado(id_estado)
-);
-
-CREATE TABLE organizador (
-    id_organizador INT PRIMARY KEY AUTO_INCREMENT,
-    nome_organizador VARCHAR(150) NOT NULL,
-    site_compra_ingresso TEXT
-);
-
-CREATE TABLE evento (
-    id_evento INT PRIMARY KEY AUTO_INCREMENT,
-    nome_evento VARCHAR(150) NOT NULL,
-    descricao_evento TEXT,
-    classificacao_etaria VARCHAR(40),
-    tipo_evento VARCHAR(100),
-    tipo_publico VARCHAR(45),
-    id_local INT NOT NULL,
-    FOREIGN KEY (id_local) REFERENCES locall(id_local)
-);
-
-CREATE TABLE edicao (
-    id_edicao INT PRIMARY KEY AUTO_INCREMENT,
-    data_inicio DATE,
-    data_fim DATE,
-    horario_inicio TIME,
-    horario_final TIME,
-    publico_realizado INT,
-    ano_referencia INT,
-    publico_esperado INT,
-    id_evento INT NOT NULL,
-    id_organizador INT NOT NULL,
-    FOREIGN KEY (id_evento) REFERENCES evento(id_evento),
-    FOREIGN KEY (id_organizador) REFERENCES organizador(id_organizador)
-);
-
-CREATE TABLE plano_turistico (
-    id_plano INT PRIMARY KEY AUTO_INCREMENT,
-    id_empresa INT NOT NULL,
-    orcamento_medio DECIMAL(10,2),
-    id_status_plano INT NOT NULL,
-    FOREIGN KEY (id_empresa) REFERENCES empresa(id_empresa),
-    FOREIGN KEY (id_status_plano) REFERENCES status_plano(id_status_plano)
-);
-
-CREATE TABLE atividade (
-    id_atividade INT PRIMARY KEY AUTO_INCREMENT,
-    ordem_no_roteiro INT,
-    id_edicao INT NOT NULL,
-    id_plano INT NOT NULL,
-    FOREIGN KEY (id_edicao) REFERENCES edicao(id_edicao),
-    FOREIGN KEY (id_plano) REFERENCES plano_turistico(id_plano)
-);
-
-CREATE TABLE pais_origem (
-    id_pais INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL UNIQUE
-);
-
-CREATE TABLE tipo_transporte (
-    id_transporte INT PRIMARY KEY AUTO_INCREMENT,
-    nome_transporte VARCHAR(50) NOT NULL UNIQUE
-);
-
-CREATE TABLE registro_turismo (
-    id_registro INT PRIMARY KEY AUTO_INCREMENT,
-    data_registro DATE NOT NULL,
-    quantidade_turistas INT NOT NULL,
-    id_pais INT NOT NULL,
-    id_transporte INT NOT NULL,
-    id_estado INT NOT NULL,
-    id_empresa INT NOT NULL,
-    FOREIGN KEY (id_pais) REFERENCES pais_origem(id_pais),
-    FOREIGN KEY (id_transporte) REFERENCES tipo_transporte(id_transporte),
-    FOREIGN KEY (id_estado) REFERENCES estado(id_estado),
-    FOREIGN KEY (id_empresa) REFERENCES empresa(id_empresa)
-);
-
-CREATE TABLE log (
+CREATE TABLE log(
     id_log INT PRIMARY KEY AUTO_INCREMENT,
     data_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
     statuss VARCHAR(30),
@@ -143,36 +36,135 @@ CREATE TABLE log (
     descricao TEXT
 );
 
-INSERT INTO status_plano (nome)
-VALUES ('Pendente'), ('Aprovado'), ('Rejeitado');
-
-INSERT INTO empresa (
-    id_empresa,
-    nome_fantasia,
-    razao_social,
-    cnpj,
-    cep,
-    numero,
-    email_contato,
-    codigo_ativacao
-)
-VALUES (
-    1,
-    'Empresa Padrao',
-    'Empresa Padrao LTDA',
-    '00000000000000',
-    '00000-000',
-    'S/N',
-    'contato@email.com',
-    'ABC123'
+CREATE TABLE pais_origem (
+    id_pais INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL UNIQUE
 );
 
-INSERT INTO funcionario (nome, cpf, email, senha, nivel_acesso, id_empresa)
-VALUES (
-    'Funcionario Padrao',
-    '00000000000',
-    'funcionario@email.com',
-    'senha123',
-    1,
-    1
+CREATE TABLE estado (
+    id_estado INT PRIMARY KEY AUTO_INCREMENT,
+    nome_estado VARCHAR(100) NOT NULL DEFAULT 'SEM NOME',
+    UF CHAR(2) NOT NULL UNIQUE
 );
+
+CREATE TABLE via_acesso (
+    id_via INT PRIMARY KEY AUTO_INCREMENT,
+    via VARCHAR(50)
+);
+
+CREATE TABLE municipio (
+    id_municipio INT PRIMARY KEY AUTO_INCREMENT,
+    municipio VARCHAR(200) NOT NULL,
+    id_estado INT NOT NULL,
+    FOREIGN KEY (id_estado) REFERENCES estado (id_estado)
+);
+
+CREATE TABLE status_plano (
+    id_status_plano INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE classificacao_etaria (
+    id_classificacao INT PRIMARY KEY AUTO_INCREMENT,
+    classificacao VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE plano_turistico (
+    id_plano INT PRIMARY KEY AUTO_INCREMENT,
+    dt_inicio DATE NOT NULL,
+    dt_fim DATE NOT NULL,
+    id_empresa INT NOT NULL,
+    id_status_plano INT NOT NULL,
+    FOREIGN KEY (id_empresa) REFERENCES empresa (id_empresa),
+    FOREIGN KEY (id_status_plano) REFERENCES status_plano (id_status_plano)
+);
+
+CREATE TABLE evento (
+    id_evento INT PRIMARY KEY AUTO_INCREMENT,
+    nome_evento VARCHAR(200) NOT NULL,
+    descricao_evento TEXT,
+    id_municipio INT NOT NULL,
+    id_classificacao INT NOT NULL,
+    FOREIGN KEY (id_municipio) REFERENCES municipio (id_municipio),
+    Foreign Key (id_classificacao) REFERENCES classificacao_etaria (id_classificacao)
+);
+
+CREATE TABLE roteiro (
+    id_roteiro INT,
+    id_plano INT,
+    id_evento INT,
+    Foreign Key (id_plano) REFERENCES plano_turistico (id_plano),
+    Foreign Key (id_evento) REFERENCES evento (id_evento),
+    CONSTRAINT pk_roteiro PRIMARY KEY (
+        id_roteiro,
+        id_plano,
+        id_evento
+    )
+);
+
+CREATE TABLE organizador (
+    id_organizador INT PRIMARY KEY AUTO_INCREMENT,
+    nome_organizador VARCHAR(200) NOT NULL,
+    site_compra_ingresso TEXT
+);
+
+CREATE TABLE edicao (
+    id_edicao INT PRIMARY KEY AUTO_INCREMENT,
+    dt_inicio DATE,
+    dt_fim DATE,
+    hr_inicio TIME,
+    hr_final TIME,
+    ano_realizacao CHAR(4),
+    publico_esperado INT,
+    publico_atingido INT,
+    id_evento INT NOT NULL,
+    id_organizador INT NOT NULL,
+    FOREIGN KEY (id_evento) REFERENCES evento (id_evento),
+    FOREIGN KEY (id_organizador) REFERENCES organizador (id_organizador)
+);
+
+INSERT INTO
+    status_plano (nome)
+VALUES ('Pendente'),
+    ('Aprovado'),
+    ('Rejeitado');
+
+INSERT INTO
+    empresa (
+        id_empresa,
+        nome_fantasia,
+        razao_social,
+        cnpj,
+        cep,
+        numero,
+        email_contato,
+        codigo_ativacao
+    )
+VALUES (
+        1,
+        '+Tourists',
+        'PlusTourists LTDA',
+        '00000000000000',
+        '00000-000',
+        'S/N',
+        'contato@plustourists.com',
+        'touris'
+    );
+
+INSERT INTO
+    funcionario (
+        nome,
+        cpf,
+        email,
+        senha,
+        nivel_acesso,
+        id_empresa
+    )
+VALUES (
+        'Anna',
+        '00000000000',
+        'anna@plustourists.com',
+        'senha123',
+        1,
+        1
+    );

7f986405671f310b1dd8524ad2fc50d2a37140d7
Ketellyn Santos <ketellyn.santos@sptech.school>
2026-05-27 17:32:03 -0300

    fix: organização

--- etl/apache-poi/src/main/java/com/plustourists/repository/ConexaoBancoDeDados.java
+++ etl/apache-poi/src/main/java/com/plustourists/repository/ConexaoBancoDeDados.java
@@ -19,8 +19,8 @@ public class ConexaoBancoDeDados {
         String host = System.getenv("DB_HOST") != null ? System.getenv("DB_HOST") : "localhost";
         String port = System.getenv("DB_PORT") != null ? System.getenv("DB_PORT") : "3306";
         String database = System.getenv("DB_NAME") != null ? System.getenv("DB_NAME") : "plustourists";
-        String user = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "root";
-        String password = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "timao";
+        String user = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "group";
+        String password = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : "*****";
 
         // Variáveis de ambiente
         /*

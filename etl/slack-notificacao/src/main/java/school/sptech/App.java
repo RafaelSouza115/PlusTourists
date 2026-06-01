4f1aee3672bafe0703678e0be1d05b6a29a3af89
Igordiasr <igor.reis@sptech.school>
2026-06-01 19:50:25 -0300

    fix: ajustand variáveis de ambiente

--- etl/slack-notificacao/src/main/java/school/sptech/App.java
+++ etl/slack-notificacao/src/main/java/school/sptech/App.java
@@ -3,11 +3,13 @@ package school.sptech;
 public class App {
 
     public static void main(String[] args) {
-        String razaoSocial = args[0];
-        String email = args[1];
-        String codigoAtivacao = args[2];
-
-        CadastroService.cadastrarUsuario(razaoSocial, email, codigoAtivacao);
-
+        if (args.length < 3) {
+            System.out.println("Erro ao receber argumentos " + args.length);
+            throw new IndexOutOfBoundsException("Erro ao receber argumentos");
+        }
+            String razaoSocial = args[0];
+            String email = args[1];
+            String codigoAtivacao = args[2];
+            CadastroService.cadastrarUsuario(razaoSocial, email, codigoAtivacao);
     }
 }
